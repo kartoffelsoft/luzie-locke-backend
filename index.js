f5fb9e0f9f358b93c4d3dee0b57a6d846bc4796b
hkjlee <harry.the.meister@gmail.com>
2021-06-15 20:30:08 +0200

    Clean up.

--- index.js
+++ index.js
@@ -22,14 +22,5 @@ app.use(cors());
 
 app.use('/api/users', usersRoutes);
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
