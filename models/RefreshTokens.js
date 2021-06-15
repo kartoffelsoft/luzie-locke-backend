7db4e491cb8166ab45c0e106734c44160608d9b6
hkjlee <harry.the.meister@gmail.com>
2021-06-15 21:54:28 +0200

    refresh token db auto deletion.

--- models/RefreshTokens.js
+++ models/RefreshTokens.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 const refreshTokenSchema = new Schema({
   token: { type: String, required: true },
-  user: { type: mongoose.Types.ObjectId, required: true, ref: 'users' } 
+  user: { type: mongoose.Types.ObjectId, required: true, ref: 'users' },
+  createdAt: { type: Date, expires: 7 * 24 * 60 * 60, default: Date.now }
 });
 
 mongoose.model('refreshTokens', refreshTokenSchema);
